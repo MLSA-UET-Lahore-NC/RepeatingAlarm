f227fefbe8e6339eb089ccf112ad5f8f444d3021
Hammad Zafar Bawara <hammadbawara@gmail.com>
2024-10-01 22:49:08 +0500

    app init files added

---
+++ app/src/main/java/com/mlsa_uet_nc/repeatalarm/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.mlsa_uet_nc.repeatalarm
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.mlsa_uet_nc.repeatalarm.ui.theme.RepeatingAlarmTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RepeatingAlarmTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Content(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Content(modifier: Modifier) {
+    Text("Not implemented yet!", modifier = modifier)
+}
