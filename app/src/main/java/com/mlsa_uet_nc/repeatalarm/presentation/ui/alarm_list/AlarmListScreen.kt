fc345bef8ac178d5873ee09652fe3b18bbca3883
Hammad Zafar Bawara <hammadbawara@gmail.com>
2024-10-19 12:49:23 +0500

    typo fixed

--- app/src/main/java/com/mlsa_uet_nc/repeatalarm/presentation/ui/alarm_list/AlarmListScreen.kt
+++ app/src/main/java/com/mlsa_uet_nc/repeatalarm/presentation/ui/alarm_list/AlarmListScreen.kt
@@ -1,4 +1,4 @@
-package com.mlsa_uet_nc.repeatalarm.presentation.ui.scheduled_list
+package com.mlsa_uet_nc.repeatalarm.presentation.ui.alarm_list
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
--- app/src/main/java/com/mlsa_uet_nc/repeatalarm/presentation/ui/navigation/NavHostContainer.kt
+++ app/src/main/java/com/mlsa_uet_nc/repeatalarm/presentation/ui/navigation/NavHostContainer.kt
@@ -5,7 +5,7 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.mlsa_uet_nc.repeatalarm.presentation.ui.scheduled_list.AlarmListScreen
+import com.mlsa_uet_nc.repeatalarm.presentation.ui.alarm_list.AlarmListScreen
 import com.mlsa_uet_nc.repeatalarm.presentation.ui.alarm_quick.QuickScreen
 import com.mlsa_uet_nc.repeatalarm.presentation.ui.settings.SettingsScreen
 
